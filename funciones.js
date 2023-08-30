4cae1e666bc07a470cce89250c604303cc88abee
Inés Rozankovic <103336169+inerozan@users.noreply.github.com>
2023-08-30 19:20:59 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function inicioYcreditos(){
+    image(playa, 0, 0, 1200, 700);
+    image(introliloystitch,  150, 210, 290, 290);
+    image(titulos, 40, 30, 500, 150); 
+    textFont(fuenteNueva); 
+    fill(0);
+    textSize(45);
+    textAlign(CENTER); 
+}
