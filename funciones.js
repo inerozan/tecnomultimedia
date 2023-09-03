4c84d6b6bc419411b3586e7aaa6a9b4496c322bf
Inés Rozankovic <103336169+inerozan@users.noreply.github.com>
2023-09-03 17:59:42 -0300

    Delete funciones.js

--- funciones.js
+++
@@ -1,9 +0,0 @@
-function inicioYcreditos(){
-    image(playa, 0, 0, 1200, 700);
-    image(introliloystitch,  150, 210, 290, 290);
-    image(titulos, 40, 30, 500, 150); 
-    textFont(fuenteNueva); 
-    fill(0);
-    textSize(45);
-    textAlign(CENTER); 
-}
